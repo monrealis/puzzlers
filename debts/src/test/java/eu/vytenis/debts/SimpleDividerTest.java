97d75a9673a70773d63e1de0b705c595de37feaa
Vytenis Bivainis <monrealis@gmail.com>
2016-03-25 00:58:18 +0200

    Add second test.

--- debts/src/test/java/eu/vytenis/debts/SimpleDividerTest.java
+++ debts/src/test/java/eu/vytenis/debts/SimpleDividerTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 // http://www.biu.ac.il/soc/ec/jlwecon/wp/2.%20AumannGame%20-%20bulletin.pdf
 public class SimpleDividerTest {
 	private Number estate = new BigDecimal("66.66");
-	private Number debt = new BigDecimal(100);
+	private Number debt1 = new BigDecimal(100);
 	private Number debt2 = new BigDecimal(300);
 	private Number payment1;
 	private Number payment2;
@@ -19,16 +19,37 @@ public class SimpleDividerTest {
 	@Test
 	public void example1() {
 		split();
-		assertEquals(payment1.toString(), "33.33");
-		assertEquals(payment2.toString(), "33.33");
+		assertEquals("33.33", payment1.toString());
+		assertEquals("33.33", payment2.toString());
+	}
+
+	@Test
+	public void example2() {
+		estate = 125;
+		split();
+		assertEquals("50", payment1.toString());
+		assertEquals("75", payment2.toString());
 	}
 
 	private void split() {
-		payment1 = estate().divide(new BigDecimal(2));
-		payment2 = estate().divide(new BigDecimal(2));
+		BigDecimal shared = debt1().min(debt2()).min(estate());
+		BigDecimal sharedPart = shared.divide(new BigDecimal(2));
+		BigDecimal estateLeft = estate().subtract(shared);
+		BigDecimal additionalPart1 = estateLeft.min(debt1().subtract(shared));
+		BigDecimal additionalPart2 = estateLeft.min(debt2().subtract(shared));
+		payment1 = sharedPart.add(additionalPart1);
+		payment2 = sharedPart.add(additionalPart2);
 	}
 
 	private BigDecimal estate() {
 		return new BigDecimal(estate.toString());
+	}
+
+	private BigDecimal debt1() {
+		return new BigDecimal(debt1.toString());
+	}
+
+	private BigDecimal debt2() {
+		return new BigDecimal(debt2.toString());
 	}
 }
