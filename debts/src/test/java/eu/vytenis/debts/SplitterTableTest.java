7e51424b0af737d690a08da18e5f37183e19bc5d
Vytenis Bivainis <monrealis@gmail.com>
2016-03-27 23:08:49 +0300

    Implement all cases in table.

--- debts/src/test/java/eu/vytenis/debts/SplitterTableTest.java
+++ debts/src/test/java/eu/vytenis/debts/SplitterTableTest.java
@@ -1,6 +1,5 @@
 package eu.vytenis.debts;
 
-import org.apache.commons.math3.fraction.Fraction;
 import org.junit.Test;
 
 public class SplitterTableTest extends BaseSplitterTest {
@@ -9,8 +8,74 @@ public class SplitterTableTest extends BaseSplitterTest {
 	}
 
 	@Test
-	public void row1() {
-		estate = new Fraction(50, 1);
+	public void row50() {
+		estate = fraction(50);
 		splitAssert("16 2 / 3, 16 2 / 3, 16 2 / 3");
+	}
+
+	@Test
+	public void row100() {
+		estate = fraction(100);
+		splitAssert("33 1 / 3, 33 1 / 3, 33 1 / 3");
+	}
+
+	@Test
+	public void row150() {
+		estate = fraction(150);
+		splitAssert("50, 50, 50");
+	}
+
+	@Test
+	public void row200() {
+		estate = fraction(200);
+		splitAssert("50, 75, 75");
+	}
+
+	@Test
+	public void row250() {
+		estate = fraction(250);
+		splitAssert("50, 100, 100");
+	}
+
+	@Test
+	public void row300() {
+		estate = fraction(300);
+		splitAssert("50, 100, 150");
+	}
+
+	@Test
+	public void row350() {
+		estate = fraction(350);
+		splitAssert("50, 100, 200");
+	}
+
+	@Test
+	public void row400() {
+		estate = fraction(400);
+		splitAssert("50, 125, 225");
+	}
+
+	@Test
+	public void row450() {
+		estate = fraction(450);
+		splitAssert("50, 150, 250");
+	}
+
+	@Test
+	public void row500() {
+		estate = fraction(500);
+		splitAssert("66 2 / 3, 166 2 / 3, 266 2 / 3");
+	}
+
+	@Test
+	public void row550() {
+		estate = fraction(550);
+		splitAssert("83 1 / 3, 183 1 / 3, 283 1 / 3");
+	}
+
+	@Test
+	public void row600() {
+		estate = fraction(600);
+		splitAssert("100, 200, 300");
 	}
 }
