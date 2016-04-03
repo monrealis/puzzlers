30cc0ca3e8c8870dde3f47682d41732f552b8ea4
Vytenis Bivainis <monrealis@gmail.com>
2016-04-04 01:16:40 +0300

    Add tests with equal claims.

---
+++ debts/src/test/java/eu/vytenis/debts/SplitterOtherCasesTest.java
@@ -0,0 +1,41 @@
+package eu.vytenis.debts;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.commons.math3.fraction.Fraction;
+import org.junit.Test;
+
+public class SplitterOtherCasesTest extends BaseSplitterTest {
+	@Test
+	public void splits500For3() {
+		estate = fraction(500);
+		debts = fractions(200, 200, 200);
+		splitAssert("166 2 / 3, 166 2 / 3, 166 2 / 3");
+	}
+
+	@Test
+	public void splits500For4() {
+		estate = fraction(500);
+		debts = fractions(200, 300, 300, 350);
+		split();
+		assertPaymentsAreInAscendingOrder();
+		assertAllEstateDivided();
+	}
+
+	private void assertPaymentsAreInAscendingOrder() {
+		for (int i = 0; i < payments.length - 1; ++i)
+			assertTrue(payments[i].compareTo(payments[i + 1]) <= 0);
+	}
+
+	private void assertAllEstateDivided() {
+		assertEquals(estate, getSumOfPayments());
+	}
+
+	private Fraction getSumOfPayments() {
+		Fraction sum = Fraction.ZERO;
+		for (Fraction p : payments)
+			sum = sum.add(p);
+		return sum;
+	}
+}
