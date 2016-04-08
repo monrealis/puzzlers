0d37b348361a49b1e4cb48c2768ef0e716ce1e7d
Vytenis Bivainis <monrealis@gmail.com>
2016-04-09 00:04:59 +0300

    Use one method instead of two.

--- debts/src/test/java/eu/vytenis/debts/SplitterBlogPostTest.java
+++ debts/src/test/java/eu/vytenis/debts/SplitterBlogPostTest.java
@@ -11,28 +11,24 @@ public class SplitterBlogPostTest extends SplitterFixture {
 	@Test
 	public void enoughForAll() {
 		estate = fraction(500);
-		split();
-		assertPayments("100, 300");
+		splitAssert("100, 300");
 	}
 
 	@Test
 	public void example1() {
 		estate = new Fraction(2, 3).add(66);
-		split();
-		assertPayments("33 1 / 3, 33 1 / 3");
+		splitAssert("33 1 / 3, 33 1 / 3");
 	}
 
 	@Test
 	public void example2() {
 		estate = new Fraction(125, 1);
-		split();
-		assertPayments("50, 75");
+		splitAssert("50, 75");
 	}
 
 	@Test
 	public void example3() {
 		estate = new Fraction(200);
-		split();
-		assertPayments("50, 150");
+		splitAssert("50, 150");
 	}
 }
