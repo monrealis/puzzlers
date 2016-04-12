3a73b8d6460fe758763aeee00e6ab39be398f9ae
Vytenis Bivainis <monrealis@gmail.com>
2016-04-13 00:11:41 +0300

    Change visibilities of methods.

--- debts/src/test/java/eu/vytenis/debts/SplitterFixture.java
+++ debts/src/test/java/eu/vytenis/debts/SplitterFixture.java
@@ -12,41 +12,40 @@ import org.apache.commons.math3.fraction.ProperFractionFormat;
 import com.google.common.base.Joiner;
 
 public abstract class SplitterFixture {
-	ProperFractionFormat format = new ProperFractionFormat();
-	Fraction estate;
-	Fraction[] claims;
-	Fraction[] payments;
+	protected ProperFractionFormat format = new ProperFractionFormat();
+	protected Fraction estate;
+	protected Fraction[] claims;
+	protected Fraction[] payments;
 
-	Fraction fraction(int sum) {
+	protected Fraction fraction(int sum) {
 		return Fraction.ZERO.add(sum);
 	}
 
-	Fraction[] fractions(int... sums) {
+	protected Fraction[] fractions(int... sums) {
 		Fraction[] r = new Fraction[sums.length];
 		for (int i = 0; i < sums.length; ++i)
 			r[i] = new Fraction(sums[i] / 1);
 		return r;
 	}
 
-	void splitAssert(String expectedPayments) {
+	protected void splitAssert(String expectedPayments) {
 		split();
 		assertPayments(expectedPayments);
 	}
 
-	void split() {
+	protected void split() {
 		payments = new Splitter(estate, claims).split();
 	}
 
-	void assertPayments(String expectedPayments) {
+	private void assertPayments(String expectedPayments) {
 		List<String> s = stream(payments).map(this::format).collect(toList());
 		String actual = Joiner.on(", ").join(s);
 		assertEquals(expectedPayments, actual);
 	}
 
-	String format(Fraction fraction) {
+	private String format(Fraction fraction) {
 		if (fraction.getNumerator() % fraction.getDenominator() == 0)
 			return fraction.toString();
 		return format.format(fraction);
 	}
-
 }
