cb4bb1af1853ee0b4616bcc53b2721942affb4a2
Vytenis Bivainis <monrealis@gmail.com>
2016-11-25 01:15:09 +0200

    Refactor.

--- galeshapley/src/test/java/eu/vytenis/galeshapley/Matcher.java
+++ galeshapley/src/test/java/eu/vytenis/galeshapley/Matcher.java
@@ -1,12 +1,15 @@
 package eu.vytenis.galeshapley;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Matcher {
 	private final int[][] preferencesOfMen;
 	private final int[][] preferencesOfWomen;
-	private final List<Integer> womanTaken = new ArrayList<>();
+	private final Set<Integer> menTaken = new HashSet<>();
+	private final Set<Integer> womenTaken = new HashSet<>();
 	private final List<Pair> result = new ArrayList<>();
 
 	public Matcher(int[][] preferencesOfMen, int[][] preferencesOfWomen) {
@@ -15,24 +18,46 @@ public class Matcher {
 	}
 
 	public List<Pair> match() {
-		for (int i = 0; i < n(); ++i) {
-			for (int j = 0; j < n(); ++j) {
-				int indexOfWoman = preferencesOfMen[i][j];
-				if (!womanTaken.contains(indexOfWoman)) {
-					addPair(i, indexOfWoman);
-					break;
-				}
+		while (true) {
+			try {
+				matchNext();
+			} catch (AllMenTaken e) {
+				return result;
 			}
 		}
-		return result;
+	}
+
+	private void matchNext() throws AllMenTaken {
+		int i = nextFreeMan();
+		for (int j = 0; j < n(); ++j) {
+			int indexOfWoman = preferencesOfMen[i][j];
+			if (!womenTaken.contains(indexOfWoman)) {
+				addPair(i, indexOfWoman);
+				break;
+			}
+		}
+	}
+
+	private int nextFreeMan() throws AllMenTaken {
+		for (int i = 0; i < n(); ++i)
+			if (!menTaken.contains(i))
+				return i;
+		throw new AllMenTaken();
+
 	}
 
 	private void addPair(int indexOfMan, int indexOfWoman) {
 		result.add(new Pair(indexOfMan, indexOfWoman));
-		womanTaken.add(indexOfWoman);
+		menTaken.add(indexOfMan);
+		womenTaken.add(indexOfWoman);
 	}
 
 	private int n() {
 		return preferencesOfMen.length;
+	}
+
+	private static class AllMenTaken extends Exception {
+		private static final long serialVersionUID = 1L;
+
 	}
 }
