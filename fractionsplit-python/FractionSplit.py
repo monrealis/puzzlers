3e1bf766eda8c18057614c348d1d8b413d489e9b
Vytenis Bivainis <monrealis@gmail.com>
2016-12-02 18:32:05 +0200

    Refactor.

--- fractionsplit-python/FractionSplit.py
+++ fractionsplit-python/FractionSplit.py
@@ -1,12 +1,22 @@
+delta = 0.000000000001
+
 class FractionSplit:
 	def getSum(self, n, d):
-		fractions = [];
-		a = 1.0 * n / d 
-		delta = 0.0000000001
-		while abs(a) > delta:
-			c = int(1 /a)
-			if (1 / a - int(1 / a) > delta):
-				c += 1;			
-			a = a - 1.0 / c
-			fractions.append('%d/%d' % (1, c))
-		return fractions
+		self.fractions = []
+		self.fillFractions(1.0 * n / d)
+		return self.fractions
+
+	def fillFractions(self, remaining):
+		while remaining > delta:
+			c = self.getLargestDenominatorWithNominatorOne(remaining)
+			remaining = remaining - 1.0 / c
+			self.fractions.append('%d/%d' % (1, c))
+
+	def getLargestDenominatorWithNominatorOne(self, upperBound):
+		inverse = 1 / upperBound
+		c = int(inverse)
+		if (inverse - c > delta):
+			return c + 1
+		else:
+			return c
+
